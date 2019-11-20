21dd84301e0e90398aa4cb270e83002ca268e2c5
shimasan <kkss07020@gmail.com>
2019-11-20 11:47:30 +0900

    [Fix] Url name

--- simple_qrcode_generated.py
+++ simple_qrcode_generated.py
@@ -1,4 +1,4 @@
 import qrcode
 
-img = qrcode.make('https://villager-b.github.io/Simple_AR_card/')
+img = qrcode.make('https://villager-b.github.io/Simple_AR_CARD/')
 img.save('img/simple_qrcode.png')
